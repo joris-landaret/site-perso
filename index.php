c6a87040dfb046b835e9def819f8503dde9dca35
joris-landaret <joris.landaret@laplateforme.io>
2023-01-25 14:53:46 +0100

    index.php : ajout paragraphe à propos de moi

--- index.php
+++ index.php
@@ -17,8 +17,19 @@
     </header>
 
     <main>
-        <h1>Joris Landaret</h1>
-        <div></div>
+        <h2>Joris Landaret</h2>
+        <h1>Recherche alternance</h1>
+        <h2>développeur web</h2>
+        <div>
+            <h2>A propos de mois</h2>
+            <p>
+                Étudiant de la formation START - WEB
+                à la Plateforme, la nouvelle école du
+                numérique de la métropole Aix-Marseille.
+                Formation de développeur web/web mobile.
+                Pédagogie active basée sur des projets.
+            </p>
+        </div>
         <div></div>
         <div></div>
     </main>
