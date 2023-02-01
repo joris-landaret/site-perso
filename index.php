9909f0d76970d5859d971eb9b28eaf1bda95b435
joris-landaret <joris.landaret@laplateforme.io>
2023-02-01 14:15:00 +0100

    Ajout du projet livre'd'or

--- index.php
+++ index.php
@@ -36,21 +36,25 @@
         <div id="projets">
             <div class="mes_projets">
                 <h3 class="titre">Site de voyages</h3>
-                <a href="https://joris-landaret.students-laplateforme.io/voyages/voyages/grece.html"><img src="img/Capture d'écran_20230127_093550.png" alt=""></a>
+                <a href="https://joris-landaret.students-laplateforme.io/voyages/voyages/grece.html" target="_blank"><img src="img/Capture d'écran_20230127_093550.png" alt=""></a>
                 <p class="langue">langage utilisé : HTML et CSS</p>
-                <p>Projet en groupe. On s'est chacun occupé de la page d'un pays, j'ai personnellement fait celui de la grèce </p>
+                <p>
+                    Projet en groupe. On s'est chacun occupé de la page d'un pays, j'ai personnellement fait celui de la grèce </p>
+            </div>
+
+            <div class="mes_projets">
+                <h3 class="titre">Livre d'or</h3>
+                <a href="http://livre-or/" target="_blank"><img src="img/livre-or.png" alt=""></a>
+                <p class="langue">langage utilisé : HTML, CSS, PHP et SQL(MariaDB)</p>
+                <p>
+                    Projet en autonomie.
+                    Création d'une base de données et des tables pour les lier aux formulaire de connexion, inscription, commentaire et livre d'or avec PHP.
+                </p>
             </div>
 
             <div class="mes_projets">
                 <h3 class="titre">titre</h3>
-                <a href=""><img src="" alt="">image / lien</a>
-                <p class="langue">langage utilisé</p>
-                <p>description projet</p>
-            </div>
-
-            <div class="mes_projets">
-                <h3 class="titre">titre</h3>
-                <a href=""><img src="" alt="">image / lien</a>
+                <a href="" target="_blank"><img src="" alt="">image / lien</a>
                 <p class="langue">langage utilisé : </p>
                 <p>description projet</p>
             </div>
